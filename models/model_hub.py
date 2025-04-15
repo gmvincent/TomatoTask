31ecd5d7167433cf53fff04050de207624c010ee
gmvincent <gmvincen@ncsu.edu>
2025-04-15 17:06:03 -0400

    fixed bugs with torch get_models(), added num_classes, and fixed args.target_layers bug

--- models/model_hub.py
+++ models/model_hub.py
@@ -2,7 +2,8 @@ import os
 import json
 
 import torch
-from torchvision.models import get_model, get_model_weights
+import torchvision.models as models
+#from torchvision.models import get_model, get_model_weights
 
 model_file = os.path.join(os.path.dirname(__file__), "torch_hub_models.json")
 torch_hub_models = json.load(open(model_file, "r"))
@@ -17,15 +18,15 @@ def get_model(args, model_name, single_task=True):
     
     # load model and weights
     if isinstance(args.pretrained, bool) and args.pretrained:
-        weights_enum = get_model_weights(model_name)
+        weights_enum = models.get_model_weights(model_name)
         weight_class = getattr(weights_enum, torch_hub_models[model_name]["weights"])
-        model = get_model(model_name, weights=weight_class)
+        model = models.get_model(model_name, weights=weight_class, num_classes=args.num_classes)
     
     elif isinstance(args.pretrained, bool) and not args.pretrained:
-        model = get_model(model_name, weights=None)
+        model = models.get_model(model_name, weights=None, num_classes=args.num_classes)
     
     elif isinstance(args.pretrained, str) and os.path.isfile(args.pretrained):
-        model = get_model(model_name, weights=None)
+        model = models.get_model(model_name, weights=None, num_classes=args.num_classes)
         checkpoint = torch.load(args.pretrained, map_location='cpu')
         if 'state_dict' in checkpoint:
             model.load_state_dict(checkpoint['state_dict'])
@@ -47,22 +48,22 @@ def get_model(args, model_name, single_task=True):
 def get_target_layer(args, model_name, model):
     
     if "fasterrcnn" in model_name:
-        args.target_layer = model.backbone
+        args.target_layers = model.backbone
     elif "resnet" in model_name:
-        args.target_layer = model.layer4[-1]
+        args.target_layers = model.layer4[-1]
     elif "vgg" in model_name:
-        args.target_layer = model.features[-1]
+        args.target_layers = model.features[-1]
     elif "dense" in model_name:
-        args.target_layer = model.features[-1]
+        args.target_layers = model.features[-1]
     elif "mobile" in model_name:
-        args.target_layer = model.features[-1]
+        args.target_layers = model.features[-1]
     elif "mnasnet" in model_name:
-        args.target_layer = model.layers[-1]
+        args.target_layers = model.layers[-1]
     elif model_name.starts_with("vit"):
-        args.target_layer = model.blocks[-1].norm1
+        args.target_layers = model.blocks[-1].norm1
     elif model_name.starts_with("swin"):
-        args.target_layer = model.layers[-1].blocks[-1].norm1
+        args.target_layers = model.layers[-1].blocks[-1].norm1
     elif "efficient" in model_name:
-        args.target_layer = model.features[-1][0]
+        args.target_layers = model.features[-1][0]
     else:
         raise ValueError(f"Model '{model_name}' does not have a predefined target_layer for GradCAM visualization.")
